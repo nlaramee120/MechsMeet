cce49680c4eeb3eb46034eb763c07baef8900562
James Capadocia <james.capadocia@gmail.com>
2022-01-09 14:16:24 -0600

    Adjusted server files to be more in line with the purpose of our project.

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,13 +1,29 @@
 const db = require('../config/connection');
-const { Tech } = require('../models');
-
-const techData = require('./techData.json');
+const { Profile } = require('../models');
+const profileSeeds = require('./profileSeeds.json');
 
 db.once('open', async () => {
-  await Tech.deleteMany({});
+  try {
+    await Profile.deleteMany({});
+    await Profile.create(profileSeeds);
 
-  const technologies = await Tech.insertMany(techData);
-
-  console.log('Technologies seeded!');
-  process.exit(0);
+    console.log('all done!');
+    process.exit(0);
+  } catch (err) {
+    throw err;
+  }
 });
+
+// const db = require('../config/connection');
+// const { Tech } = require('../models');
+
+// const techData = require('./techData.json');
+
+// db.once('open', async () => {
+//   await Tech.deleteMany({});
+
+//   const technologies = await Tech.insertMany(techData);
+
+//   console.log('Technologies seeded!');
+//   process.exit(0);
+// });
