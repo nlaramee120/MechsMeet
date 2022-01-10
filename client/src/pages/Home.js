74ec706bad9d094ac5ac78687f9a3ff5e767f17e
Edgar Ortega <edort93@gmail.com>
2022-01-09 21:45:12 -0600

    updated for potential use of different queries

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,22 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-import { QUERY_MATCHUPS } from '../utils/queries';
+// import { QUERY_MATCHUPS } from '../utils/queries';
+// import ProfileList from '../components/ProfileList';
+// import ProfileForm from '../components/ProfileForm';
+
+import { QUERY_PROFILES } from '../utils/queries';
+
 
 const Home = () => {
-  const { loading, data } = useQuery(QUERY_MATCHUPS, {
-    fetchPolicy: "no-cache"
-  });
+  // const { loading, data } = useQuery(QUERY_MATCHUPS, {
+  //   fetchPolicy: "no-cache"
+  // });
 
-  const matchupList = data?.matchups || [];
+  // const matchupList = data?.matchups || [];
+  const { loading, data } = useQuery(QUERY_PROFILES);
+  const profiles = data?.profiles || [];
+
 
   return (
     <div>
