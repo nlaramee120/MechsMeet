0c9c4bc219dd8c83fdc0135e75be94b7af7691aa
James Capadocia <james.capadocia@gmail.com>
2022-01-10 21:38:40 -0600

    Home page and component stylings and wire frame setup.

--- client/src/components/ProfileForm/index.js
+++ client/src/components/ProfileForm/index.js
@@ -3,6 +3,7 @@ import { useMutation } from '@apollo/client';
 
 import { ADD_PROFILE } from '../../utils/mutations';
 import { QUERY_PROFILES } from '../../utils/queries';
+import "./ProfileForm.css";
 
 const ProfileForm = () => {
   const [name, setName] = useState('');
@@ -38,23 +39,14 @@ const ProfileForm = () => {
 
   return (
     <div>
-      <h3>Add yourself to the list...</h3>
+      <h3>Are you a mechanic?</h3>
       <form
         className="flex-row justify-center justify-space-between-md align-center"
         onSubmit={handleFormSubmit}
       >
-        <div className="col-12 col-lg-9">
-          <input
-            placeholder="Add your profile name..."
-            value={name}
-            className="form-input w-100"
-            onChange={(event) => setName(event.target.value)}
-          />
-        </div>
-
         <div className="col-12 col-lg-3">
-          <button className="btn btn-info btn-block py-3" type="submit">
-            Add Profile
+          <button className="btn btn-primary" type="submit">
+            Start offering your services now!
           </button>
         </div>
         {error && (
@@ -65,6 +57,68 @@ const ProfileForm = () => {
       </form>
     </div>
   );
+
+  // const ProfileForm = () => {
+  //   const [name, setName] = useState('');
+  
+  //   const [addProfile, { error }] = useMutation(ADD_PROFILE, {
+  //     update(cache, { data: { addProfile } }) {
+  //       try {
+  //         const { profiles } = cache.readQuery({ query: QUERY_PROFILES });
+  
+  //         cache.writeQuery({
+  //           query: QUERY_PROFILES,
+  //           data: { profiles: [...profiles, addProfile] },
+  //         });
+  //       } catch (e) {
+  //         console.error(e);
+  //       }
+  //     },
+  //   });
+  
+  //   const handleFormSubmit = async (event) => {
+  //     event.preventDefault();
+  
+  //     try {
+  //       const { data } = await addProfile({
+  //         variables: { name },
+  //       });
+  
+  //       setName('');
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  //   };
+
+  // return (
+  //   <div>
+  //     <h3>Add yourself to the list...</h3>
+  //     <form
+  //       className="flex-row justify-center justify-space-between-md align-center"
+  //       onSubmit={handleFormSubmit}
+  //     >
+  //       <div className="col-12 col-lg-9">
+  //         <input
+  //           placeholder="Add your profile name..."
+  //           value={name}
+  //           className="form-input w-100"
+  //           onChange={(event) => setName(event.target.value)}
+  //         />
+  //       </div>
+
+  //       <div className="col-12 col-lg-3">
+  //         <button className="btn btn-info btn-block py-3" type="submit">
+  //           Add Profile
+  //         </button>
+  //       </div>
+  //       {error && (
+  //         <div className="col-12 my-3 bg-danger text-white p-3">
+  //           Something went wrong...
+  //         </div>
+  //       )}
+  //     </form>
+  //   </div>
+  // );
 };
 
 export default ProfileForm;
