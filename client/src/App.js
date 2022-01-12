df694eb3a0690bab450c8afdf747df26f0461109
James Capadocia <james.capadocia@gmail.com>
2022-01-11 19:43:35 -0600

    More Heroku deployment testing.

--- client/src/App.js
+++ client/src/App.js
@@ -14,29 +14,29 @@ import Footer from './components/Footer';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 
-const client = new ApolloClient({
-  uri: "/graphql",
-  cache: new InMemoryCache(),
-});
-
-// const httpLink = createHttpLink({
-//   uri: '/graphql',
-// });
-
-// const authLink = setContext((_, { headers }) => {
-//   const token = localStorage.getItem('id_token');
-//   return {
-//     headers: {
-//       ...headers,
-//       authorization: token ? `Bearer ${token}` : '',
-//     },
-//   };
-// });
-
 // const client = new ApolloClient({
-//   link: authLink.concat(httpLink),
+//   uri: "/graphql",
 //   cache: new InMemoryCache(),
 // });
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 // function App() {
 //   const [showItem, setShowItem] = useState(false)
