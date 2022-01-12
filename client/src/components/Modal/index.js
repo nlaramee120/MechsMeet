7a495d595acc58d5d0e12af888ac19208bad310c
James Capadocia <james.capadocia@gmail.com>
2022-01-12 01:28:14 -0600

    Created Modal component to enter Stripe API code into. It currently works as a new route though intentions are for it to be a modal. TBD but it will work for the MVP, lol.

---
+++ client/src/components/Modal/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Modal = () => {
+  return (
+    <div>
+      <h1>***Stripe API Goes Here***</h1>
+    </div>
+  );
+};
+
+export default Modal;
