cf9f935ed72882623c45b375703e52081b952fd7
Edgar Ortega <edort93@gmail.com>
2022-01-15 00:29:31 -0600

    still working on adding the crud, added a return for not being logged in

--- client/src/pages/myProfile.js
+++ client/src/pages/myProfile.js
@@ -1,17 +1,38 @@
-import React from "react";
+import React,{ useState}  from "react";
+import { Link } from 'react-router-dom';
 
 // Import the `useParams()` hook
 import { Redirect, useParams } from "react-router-dom";
-import { useQuery } from "@apollo/client";
+import { useQuery, useMutation } from "@apollo/client";
 
 import SkillsList from "../components/SkillsList";
 import SkillForm from "../components/SkillForm";
 import "./Pages.css";
 import Auth from "../utils/auth";
 
-import { QUERY_ME } from "../utils/queries";
+import { QUERY_ME} from "../utils/queries";
+import { UPDATE_MYP} from "../utils/mutations";
 
 const Myprofile = () => {
+  const [about, setabout] = useState('');
+  const [ updateMyProfile, { error }] = useMutation(UPDATE_MYP);
+ const handleFormSubmit=  async (event) => {
+  
+    try {
+      console.log(profileId)
+      debugger;
+      // its not pulling the profileId
+      const data = await  updateMyProfile({
+        variables: { profileId, about },
+      });
+
+      setabout('');
+     
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   // Use `useParams()` to retrieve value of the route parameter `:profileId`
   const { profileId } = useParams();
 
@@ -19,9 +40,20 @@ const Myprofile = () => {
     variables: { profileId: profileId },
   });
 
+
   // const profile = data?.profile || {};
   const profile = data?.me || data?.profile || {};
   console.log(data?.me || data?.profile);
+   //if not logged in
+   if (!data?.me || data?.profile) {
+    return (
+      <h4>
+        You need to be logged in to see your profile page. 
+        <br/>
+        <Link to="/login">Login Here!</Link> or <Link to="/signup">Signup Here!.</Link>
+      </h4>
+    );
+  }
   // redirect to personal profile page if username is yours
   if (Auth.loggedIn() && Auth.getProfile().data._id === profileId)
     return <Redirect to="/me:/profileId" />;
@@ -38,20 +70,39 @@ const Myprofile = () => {
   //   );
   // }
 
+  
   return (
     <div>
       <h1>My Profile</h1>
       <div className="myInfo">
         <h3>About:</h3>
-        <p className="editInfo" contenteditable="true">
-          {profile.about}
-        </p>
+     
+        <form
+          className="flex-row justify-center justify-space-between-md align-center"
+          onSubmit={handleFormSubmit}
+        >
+          <div className="col-12 col-lg-9">
+            <input
+              placeholder="About Me..."
+              value={about}
+              className="form-input w-100"
+              onChange={(event) => setabout(event.target.value)}
+            />
+          </div>
+          <div className="col-12 col-lg-3">
+            <button className="btn btn-info btn-inline py-3" type="submit">
+              Update!
+            </button>
+          </div>
+        </form>
+      
+  
         <h3>Location:</h3>
-        <p className="editInfo" contenteditable="true">
+        <p className="editInfo" contentEditable="true" suppressContentEditableWarning={true}>
           {profile.location}
         </p>
         <h3>Contact Info:</h3>
-        <p className="editInfo" contenteditable="true">
+        <p className="editInfo" contentEditable="true" suppressContentEditableWarning={true}>
           {profile.email}{" "}
         </p>
         <h3 className="card-header">Specializations and Skill Sets:</h3>
@@ -69,6 +120,7 @@ const Myprofile = () => {
           <SkillForm profileId={profile._id} />
         </div>
       </div>
+    
     </div>
   );
 };
