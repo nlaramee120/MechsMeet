f68d72df0ce21f9a92f7d55070fc4f30afbd3136
James Capadocia <james.capadocia@gmail.com>
2022-01-20 13:24:34 -0600

    Cleaned up index.js, app.js, and all pages in client directory.

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,24 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import Dropbox from '../components/Dropbox'
-
-// Import the `useParams()` hook
+import Dropbox from "../components/Dropbox";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
-
 import SkillsList from "../components/SkillsList";
 import "./Pages.css";
 
 import { QUERY_SINGLE_PROFILE } from "../utils/queries";
 
-import { genCheckup, StripeButton } from "../components/Stripe/script";
-
 const Profile = () => {
-  // Use `useParams()` to retrieve value of the route parameter `:profileId`
   const { profileId } = useParams();
 
   const { loading, data } = useQuery(QUERY_SINGLE_PROFILE, {
-    // pass URL parameter
     variables: { profileId: profileId },
   });
 
@@ -30,9 +22,9 @@ const Profile = () => {
   return (
     <div>
       <div className="payMechCont">
-      <p class="payText">Have services been completed?</p>
+        <p class="payText">Have services been completed?</p>
 
-      <Dropbox />
+        <Dropbox />
       </div>
 
       <div className="mechInfo">
@@ -43,15 +35,16 @@ const Profile = () => {
         <h3>Location:</h3>
         <p>{profile.location}</p>
         <h3>Contact Info:</h3>
-        <p>{profile.email}<br></br>{profile.phone} </p>
+        <p>
+          {profile.email}
+          <br></br>
+          {profile.phone}{" "}
+        </p>
         <h3>Specializations and Skill Sets</h3>
 
         {profile.skills?.length > 0 && (
-        <SkillsList
-          skills={profile.skills}
-          isLoggedInUser={false}
-        />
-      )}
+          <SkillsList skills={profile.skills} isLoggedInUser={false} />
+        )}
       </div>
     </div>
   );
